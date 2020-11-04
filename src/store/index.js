355b11839686a387df39eb2513d00db2ffd9674e
On <5586237+wm1045266943@user.noreply.gitee.com>
2020-11-04 15:01:40 +0800

    '初始化项目'

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+//
+import actions from './actions'
+import mutations from './mutations'
+import modules from './modules'
+import getters from './getters'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  mutations,
+  actions,
+  modules,
+  getters
 })
